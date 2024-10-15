ece75b1731def8182083d6dc28dcf013050cdcd6
nafiuibnemazhar <nafiuibnemazhar0@gmail.com>
2024-10-15 20:52:39 +0600

    Turned pet card into a JS template

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
 async function start() {
   const weatherPromise = await fetch(
     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
@@ -16,7 +19,13 @@ async function petsArea() {
   );
   const petsData = await petsPromise.json();
   petsData.forEach((pet) => {
-    console.log(pet.species);
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-description").textContent = pet.description;
+
+    wrapper.appendChild(clone);
   });
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 petsArea();
